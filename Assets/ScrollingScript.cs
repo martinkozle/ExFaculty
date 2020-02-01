30ad60b49074bed8794426f3d0b50ea20c6f725c
martinkozle <martinkozle@users.noreply.github.com>
2020-02-01 01:30:34 +0100

    added new sprites and changed filter mode

---
+++ Assets/ScrollingScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ScrollingScript : MonoBehaviour
+{
+    private float camerax;
+    private float cameray;
+
+    // Update is called once per frame
+    void Update () {
+        camerax = Camera.main.transform.position.x;
+        cameray = Camera.main.transform.position.y;
+        transform.position = new Vector3(camerax * 0.9f , cameray * 0.9f + 1, transform.position.z);
+    }
+}
